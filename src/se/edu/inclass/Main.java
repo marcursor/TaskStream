374dfdc5f6dc89118de055a3bf77e4791e9df758
MarcusTan <marcus.tan.598@gmail.com>
2020-09-18 17:40:22 +0800

    Add lambda examples

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -8,6 +8,8 @@ import se.edu.inclass.task.TaskNameComparator;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
+
 public class Main {
 
     private TaskNameComparator taskNameComparator;
@@ -22,6 +24,11 @@ public class Main {
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+        printSortedDeadlinesUsingStreams(tasksData);
+
+        for(Task t: filterByString(tasksData, "11")) {
+            System.out.println(t);
+        }
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
@@ -46,5 +53,21 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+
+    public static void printSortedDeadlinesUsingStreams(ArrayList<Task> tasksData) {
+        System.out.println("Printing sorted deadline using Streams and lambdas:");
+        tasksData.stream()
+                .filter((s) -> s instanceof Deadline)
+                .sorted((a,b) -> a.getDescription().toLowerCase().compareTo(b.getDescription().toLowerCase()))
+                .forEach(System.out::println);
+    }
+
+    public static ArrayList<Task> filterByString(ArrayList<Task> tasksData, String filterString) {
+        System.out.println("Filtering by string:");
+        ArrayList<Task> filteredTaskList = (ArrayList<Task>) tasksData.stream()
+                .filter((s) -> s.getDescription().contains(filterString))
+                .collect(toList());
+        return filteredTaskList;
     }
 }
